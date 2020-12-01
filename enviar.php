1315f46750151d83c4f36e67df5c19ac2b583cc4
Pasteleria <74636441+Chaco0530@users.noreply.github.com>
2020-11-30 21:07:42 -0600

    Add files via upload

--- enviar.php
+++ enviar.php
@@ -1,10 +1,9 @@
 <?php
-$destino="chaco.cm2001@gmail.com";
 $nombre = $_POST["nombre"];
 $correo = $_POST["correo"];
 $telefono = $_POST["telefono"];
 $mensaje = $_POST["mensaje"];
-$contenido ="Nombre" . $nombre "\nCorreo: " . $correo . "\nTeléfono: " . $telefono . "\nMensaje: " .$mensaje;
-mail($destino,"Cotización",$contenido);
-header("Location: /gracias.html",TRUE,301);
+$contenido ="Nombre" .$nombre "\nCorreo: " .$correo  "\nTeléfono: "  .$telefono  "\nMensaje: " .$mensaje;
+mail(isaac.cm2001@gmail.com,"Cotización",$contenido);
+header("Location:/gracias.html",TRUE,301);
 ?>
